a4c32449677a051b6c8a1c5bcd4d90684ea64331
Pierre <pierre@delange.design>
2022-05-29 14:17:56 -0700

    Add project files.

---
+++ MauiApp2/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp2.Views;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+}
+
---
+++ MauiApp2/Views/RadioButtons2.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiApp2.Views;
+
+public partial class RadioButtons2 : ContentPage
+{
+	public RadioButtons2()
+	{
+		InitializeComponent();
+	}
+
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+}
